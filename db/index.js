3a65d42b4ee40b9362f34d5c80460ebeb6b781ac
hjdtl <873804682@qq.com>
2020-09-30 12:56:29 +0800

    更新数据库配置

--- db/index.js
+++ db/index.js
@@ -2,7 +2,14 @@ const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize('mysql', 'root', '123456', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '+08:00',
+    define: {
+      charset: 'utf8',
+      dialectOptions: {
+        collate: 'utf8_general_ci'
+      }
+    }
 })
 
 module.exports = sequelize
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 solution: 
 
 ```shell
+# 创建数据库
+ CREATE DATABASE IF NOT EXISTS hm DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+
+# 生成初始数据
 PS D:\lib\hrmanage-serve> ./node_modules/.bin/sequelize db:seed
 
 Sequelize CLI [Node: 12.14.0, CLI: 6.2.0, ORM: 6.3.4]
